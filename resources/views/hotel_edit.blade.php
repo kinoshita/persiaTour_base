8bf8fd2d0411bc1d51bae6a994182f412c2ab940
木下恵介 <kinoshita@kinoshitakeisukenoMacBook-Air.local>
2024-07-19 12:11:16 +0900

    commit

--- resources/views/hotel_edit.blade.php
+++ resources/views/hotel_edit.blade.php
@@ -12,30 +12,37 @@
     <div class="w-full flex justify-center">
         <form class="shadow-md rounded-b-md bg-white w-full max-w-2xl p-2 " method="post" action="{{ route('hotel.update') }}">
             @csrf
-            @if($errors)
-                <div>
-
-                    {{$errors}}
-                </div>
-            @endif
+            <div>
+                <span class="flex justify-center text-red-600">
+                    @if($errors->has('city_name'))
+                        {{$errors->first('city_name')}}
+                    @endif
+                </span>
+            </div>
             <div class="flex sm:items-center mb-6 flex-col sm:flex-row">
+
                 <label for="city_name" class="block sm:w-2/5 font-bold sm:text-right mb-1 pr-4">
                     CITY:<span class="p-1 ml-2 bg-red-500 rounded text-gray-200">required</span>
                 </label>
                 <input class="block w-full sm:w-3/5 py-2 px-3 text-gray-700 border border-gray-200 rounded focus:outline-none focus:bg-white" id="city_name" name="city_name" value="{{old('city_name',$hotel->city)}}"/>
             </div>
+
+
+
+
             <div class="flex sm:items-center mb-6 flex-col sm:flex-row">
                 <label for="hotel_name" class="block sm:w-2/5 font-bold sm:text-right mb-1 pr-4">
                     Hotel Name:<span class="p-1 ml-2 bg-red-500 rounded">required</span>
                 </label>
                 <input class="block w-full sm:w-3/5 py-2 px-3 text-gray-700 border border-gray-200 rounded focus:outline-none focus:bg-white"  id="hotel_name" name="hotel_name" value="{{$hotel->hotel_name}}" />
             </div>
+
             <div class="flex sm:items-center mb-6 flex-col sm:flex-row">
                 <label for="address" class="block sm:w-2/5 font-bold sm:text-right mb-1 pr-4">
                     Address:<span class="p-1 ml-2 bg-red-500 rounded">required</span>
                 </label>
 
-                <input class="block w-full sm:w-3/5 py-2 px-3 text-gray-700 border border-gray-200 rounded focus:outline-none focus:bg-white"  id="address" name="address" value="{{$hotel->address}}" />
+                <input class="block w-full sm:w-3/5 py-2 px-3 text-gray-700 border border-gray-200 rounded focus:outline-none focus:bg-white"  id="address" name="address" value="{{old('address',$hotel->address)}}" />
             </div>
             <div class="flex sm:items-center mb-6 flex-col sm:flex-row">
                 <label for="tel" class="block sm:w-2/5 font-bold sm:text-right mb-1 pr-4">
